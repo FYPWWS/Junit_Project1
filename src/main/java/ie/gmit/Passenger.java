10f453a8a5d359c66897a3397f46bf0362dc25a3
SIMON GABORIT - STUDENT <G00377155@gmit.ie>
2022-02-15 09:40:56 +0000

    Update day 2, failure test 1

--- src/main/java/ie/gmit/Passenger.java
+++ src/main/java/ie/gmit/Passenger.java
@@ -2,8 +2,39 @@ package ie.gmit;
 
 public class Passenger
 {
+    String Title;
+    String Name;
+    String ID;
+    String Number;
+    String Age;
+
     public Passenger(String Title, String Name, String ID, String Phone, String Age)
     {
 
+    }
+
+    public String getTitle()
+    {
+        return Title;
+    }
+
+    public String getName()
+    {
+        return Name;
+    }
+
+    public String getID()
+    {
+        return ID;
+    }
+
+    public String getNumber()
+    {
+        return Number;
+    }
+
+    public String getAge()
+    {
+        return Age;
     }
 }
--- src/test/java/ie/gmit/TestPassenger.java
+++ src/test/java/ie/gmit/TestPassenger.java
@@ -2,25 +2,27 @@ package ie.gmit;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.omg.CORBA.portable.ApplicationException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestPassenger {
 
-    Passenger myPassenger;
-    
-    
-    @Test
-    void testConstructorValidTitle()
-    {
-
-    }
     //test negative response
     @Test
     void testConstructorInvalidTitle()
     {
-        final String invalid = "Invalid title provided";
-        Exception exceptionThrown = Assertions.assertThrows(ApplicationException.class, () -> {"", "Bob", "123456", "086 199 5980", "33"});
-        Assertions.assertEquals("Mr" || "Mrs" || "Ms");
+        final String invalid = "Invalid title provided: Mr, Mrs or Ms.";
+        Exception exceptionThrown = assertThrows(IllegalArgumentException.class, () -> new Passenger("", "Bob", "123456", "086 199 5980", "33"));
+        Assertions.assertEquals(invalid, exceptionThrown.getMessage());
     }
+
+//    @Test
+//    void testConstructorValidTitle()
+//    {
+//        final String invalid = "Valid title provided.";
+//        Exception exceptionThrown = Assertions.assertThrows(ApplicationException.class, () -> new Passenger("Mr", "Bob", "123456", "086 199 5980", "33"));
+//        Assertions.assertEquals(invalid, exceptionThrown.getMessage());
+//    }
+
 
 }
